11c94070bc12c7da2d31da1e0a3e8acb6572db85
skirianov <s.kirianov@gmail.com>
2022-10-10 16:22:51 +0100

    fixed error

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ app.get('/', (req,res) => {
   res.send('Hello World');
 });
 
-server.listen(port, () => console.log(`Example app listening on port ${process.env.PORT || 3000}!`));
+server.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT || 3000}!`));
