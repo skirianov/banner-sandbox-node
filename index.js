e491624f5941428604cba6b773cb1f2a44f940f0
skirianov <s.kirianov@gmail.com>
2022-10-10 16:08:10 +0100

    add get request on / to check if working

--- index.js
+++ index.js
@@ -2,19 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const http = require('http');
-const fs = require('fs');
 
 const { CONNECT_WITH_MONGO } = require('./db/connection');
 const { CONNECT_WITH_REDIS } = require('./cache/connect-redis');
 const routes = require('./api/squares');
-const { populateDb } = require('./utils/populateSquares');
-const { twitterClient } = require('./api/twitter');
-const { job } = require('./cron/cron-job');
 
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
 
 const server = http.createServer(app);
 const ws = require('./websocket/ws').init(server);
@@ -27,4 +22,8 @@ app.use(express.json());
 app.use(morgan('common'));
 app.use(routes);
 
-server.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req,res) => {
+  res.send('Hello World');
+});
+
+server.listen(port, () => console.log(`Example app listening on port ${process.env.PORT || 3000}!`));
