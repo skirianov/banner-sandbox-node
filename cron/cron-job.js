576aee75ee75102dea701efde66f4b9b0d5e93a1
skirianov <s.kirianov@gmail.com>
2022-10-11 17:29:07 +0100

    trying out cron job

--- cron/cron-job.js
+++ cron/cron-job.js
@@ -10,6 +10,7 @@ const job = nodeCron.schedule('*/20 * * * *', async () => {
   console.log('running a task every 20 minutes');
 
   const squares = await Square.find({});
+
   
   const imageBuffer = convertImageFromArray(squares);
   createImageFromBuffer(imageBuffer, '../assets/banner.png');
@@ -39,5 +40,35 @@ const saveBanner = nodeCron.schedule('*/5 * * * *', async () => {
 
   createImageFromBuffer(imageBuffer, '../assets/progress' + currentTime + '.png');
 })
+
+const compareSquares = nodeCron.schedule('*/2 * * * *', async () => {
+  console.log('comparing squares');
+
+  const squaresFromJson = fs.readFileSync(path.join(__dirname, 'squares.json'));
+  const squaresJSON = JSON.parse(squaresFromJson);
+
+  console.log(squaresJSON)
+});
+
+const squareData = nodeCron.schedule('*/1 * * * *', async () => {
+  console.log('saving data in json');
+
+  const squaresJson = JSON.stringify(squares);
+  fs.writeFileSync('squares.json', squaresJson);
+})
+
+// uncomment once BETA is over
+// const tweetBannerUpdate = nodeCron.schedule('0 12 */1 * *', async () => {
+//   const banner = fs.readFileSync(path.join(__dirname, '../assets/banner.png'));
+
+//   try {
+//     await twitterClient.v1.tweet({
+//       status: `New banner update! Let's see what you come up with! Want to join? https://dancing-kitsune-2b3b6a.netlify.app/`,
+//       media_ids: [banner]
+//     })
+//   } catch(error) {
+//     console.log(error);
+//   }
+// });
 
 module.exports = { job, saveBanner };
